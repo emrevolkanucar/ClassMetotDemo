4eb99ee64ef131f819f89b55c115505cc5900eed
Emre Volkan UÇAR <emrevolkanucar@gmail.com>
2021-01-20 14:37:56 +0300

    Customer class and list added.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class Musteri
+    {   
+        public int Id { get; set; }
+        public string MusteriAdi { get; set; }
+        public string MusteriSoyadi { get; set; }
+        public string Tckn { get; set; }
+        public int Yas { get; set; }
+        public string Cinsiyet { get; set; }
+        public string Uyruk { get; set; }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,43 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri()
+            {
+                Id = 1,
+                MusteriAdi = "Jack",
+                MusteriSoyadi = "Sparrow",
+                Yas = 30,
+                Cinsiyet = "Erkek",
+                Uyruk = "T.C.",
+                Tckn = "14614655580"
+            };
+
+            Musteri musteri2 = new Musteri()
+            {
+                Id = 2,
+                MusteriAdi = "Darth",
+                MusteriSoyadi = "Vader",
+                Yas = 38,
+                Cinsiyet = "Erkek",
+                Uyruk = "T.C.",
+                Tckn = "50014655580"
+            };
+
+            Musteri musteri3 = new Musteri()
+            {
+                Id = 3,
+                MusteriAdi = "Barış",
+                MusteriSoyadi = "Manço",
+                Yas = 27,
+                Cinsiyet = "Erkek",
+                Uyruk = "T.C.",
+                Tckn = "80014655580"
+            };
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+
+
         }
     }
 }
